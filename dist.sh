462de410dfe31eeebda2f88be253e8e71ae32ab4
Guenter Bartsch <guenter@zamia.org>
2021-10-06 18:29:14 +0200

    unified dist.sh

--- dist.sh
+++ dist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 VERSION=0.7.3
-LHA=aqb-${VERSION}.lha
-WORKDIR=target/m68k-amigaos/dist
+WORKDIR=`pwd`/target/m68k-amigaos/dist
+LHA=${WORKDIR}/aqb-${VERSION}.lha
 
 rm -rf ${WORKDIR}/aqb
 rm -rf ${WORKDIR}/aqb.lha
@@ -10,23 +10,27 @@ mkdir -p ${WORKDIR}/aqb/lib
 mkdir -p ${WORKDIR}/aqb/examples
 mkdir -p ${WORKDIR}/aqb/examples/bench
 mkdir -p ${WORKDIR}/aqb/examples/demo
+mkdir -p ${WORKDIR}/aqb/tutorial
 
-cp target/m68k-amigaos/bin/aqb ${WORKDIR}/aqb/
-cp src/lib/_brt/_brt.sym ${WORKDIR}/aqb/lib/
-cp src/lib/_brt/_brt.a   ${WORKDIR}/aqb/lib/
-cp src/lib/_aqb/_aqb.sym ${WORKDIR}/aqb/lib/
-cp src/lib/_aqb/_aqb.a   ${WORKDIR}/aqb/lib/
-cp src/lib/startup.o     ${WORKDIR}/aqb/lib/
-cp README.guide          ${WORKDIR}/aqb/
-cp CHANGELOG.adoc        ${WORKDIR}/aqb/CHANGELOG
+cp target/m68k-amigaos/bin/aqb        ${WORKDIR}/aqb/
+cp src/lib/_brt/_brt.sym              ${WORKDIR}/aqb/lib/
+cp src/lib/_brt/_brt.a                ${WORKDIR}/aqb/lib/
+cp src/lib/_aqb/_aqb.sym              ${WORKDIR}/aqb/lib/
+cp src/lib/_aqb/_aqb.a                ${WORKDIR}/aqb/lib/
+cp src/lib/IFFSupport/IFFSupport.sym  ${WORKDIR}/aqb/lib/
+cp src/lib/IFFSupport/IFFSupport.a    ${WORKDIR}/aqb/lib/
+cp src/lib/startup.o                  ${WORKDIR}/aqb/lib/
+cp README.guide                       ${WORKDIR}/aqb/
+cp CHANGELOG.adoc                     ${WORKDIR}/aqb/CHANGELOG
 
-cp dist/amiga/Icons/aqb_topdir.info ${WORKDIR}/aqb.info
-cp dist/amiga/Icons/aqb.info ${WORKDIR}/aqb/aqb.info
-cp dist/amiga/Icons/examples.info ${WORKDIR}/aqb/examples.info
-cp dist/amiga/Icons/bench.info ${WORKDIR}/aqb/examples/bench.info
-cp dist/amiga/Icons/demo.info ${WORKDIR}/aqb/examples/demo.info
+cp dist/amiga/Icons/aqb_topdir.info   ${WORKDIR}/aqb.info
+cp dist/amiga/Icons/aqb.info          ${WORKDIR}/aqb/aqb.info
+cp dist/amiga/Icons/examples.info     ${WORKDIR}/aqb/examples.info
+cp dist/amiga/Icons/bench.info        ${WORKDIR}/aqb/examples/bench.info
+cp dist/amiga/Icons/demo.info         ${WORKDIR}/aqb/examples/demo.info
 cp dist/amiga/Icons/README.guide.info ${WORKDIR}/aqb/
-cp dist/amiga/Icons/CHANGELOG.info ${WORKDIR}/aqb/CHANGELOG.info
+cp dist/amiga/Icons/CHANGELOG.info    ${WORKDIR}/aqb/CHANGELOG.info
+cp dist/amiga/Icons/tutorial.info     ${WORKDIR}/aqb/
 
 for EX in examples/bench/*.bas ; do
     cp $EX ${WORKDIR}/aqb/examples/bench/
@@ -38,14 +42,43 @@ for EX in examples/demo/*.bas ; do
     cp dist/amiga/Icons/`basename $EX`.info ${WORKDIR}/aqb/examples/demo/
 done
 
+for EX in tutorial/*.bas ; do
+    cp $EX ${WORKDIR}/aqb/tutorial/
+    cp dist/amiga/Icons/`basename $EX`.info ${WORKDIR}/aqb/tutorial/
+done
+cp -r tutorial/imgs ${WORKDIR}/aqb/tutorial/
+
 cp -r help ${WORKDIR}/aqb/
 
 pushd ${WORKDIR}
 lha a ${LHA} aqb.info aqb
-cp ${LHA} /home/guenter/media/emu/amiga/FS-UAE/hdd/system/x/
 popd
 
-#sudo cp -r Fonts/aqb /mnt/amiga/Fonts/
-#sudo cp Fonts/aqb.font /mnt/amiga/Fonts/
+echo "${LHA} created."
+
+LAMIGA=$XAMIGA/..
+echo cp ${LHA} $XAMIGA
+cp ${LHA} $XAMIGA
+
+if [ -e "$LAMIGA/Apps" ]; then
+
+    pushd "$LAMIGA/Apps"
+    rm -rf aqb*
+    lha x -f ${LHA}
+    popd
+
+fi
+
+EAMIGA=/mnt/amiga
+if [ -e "$EAMIGA/Apps" ]; then
+
+    cp ${LHA} $EAMIGA/x/
+    pushd "$EAMIGA/Apps"
+    rm -rf aqb*
+    lha x ${LHA}
+    popd
+
+fi
+
 
 
