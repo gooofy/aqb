7b3de2148a029b87fc4bb60419b4ad49024ace03
Guenter Bartsch <guenter@zamia.org>
2021-10-29 18:38:55 +0200

    ffptest added

---
+++ src/tools/ffptest/ffptest.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdarg.h>
+//#include <string.h>
+//#include <errno.h>
+#include <assert.h>
+//#include <math.h>
+
+#include <clib/mathffp_protos.h>
+#include <inline/mathffp.h>
+
+#include <clib/mathtrans_protos.h>
+#include <inline/mathtrans.h>
+
+#include <_brt/_brt.h>
+
+int _aqb_main (int argc, char *argv[])
+{
+    FLOAT f1 = SPFlt(10);
+    FLOAT f2 = SPFlt(42);
+
+    FLOAT f3 = f1*f2;
+    FLOAT f4 = f2/f1;
+
+    DPRINTF ("f1=%d, f2=%d, f3=%d, f4=f2/f1=%d\n", SPFix(f1), SPFix(f2), SPFix(f3), SPFix(f4));
+
+    BOOL b = f1 < f2;
+
+    DPRINTF ("b = f1 < f2 = %d\n", b);
+
+
+    return 0;
+}
+
